2837797614df9df448caf26b3c306159e4ceac3f
unknown <dhbae07@naver.com>
2025-12-05 16:24:58 +0900

    주 도메인을 www.2dto3d-khu.kr 에서 2dto3d-khu.kr 로 전환 함

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
 
   // 이미지 최적화
   images: {
-    domains: ['localhost', '2dto3d.vercel.app'],
+    domains: ['localhost', '2dto3d.vercel.app', '2dto3d-khu.kr'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
