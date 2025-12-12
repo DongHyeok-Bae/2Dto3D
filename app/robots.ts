be13ec2adaee24171eca3f8111a07c45265c7f2f
unknown <dhbae07@naver.com>
2025-12-12 21:26:06 +0900

    app/robots.ts 생성하고 기존에 있던 하드 코딩 robots.ts 삭제-> 색인 문제 해결

---
+++ app/robots.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://2dto3d-khu.kr'
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/_next/', '/static/'],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
